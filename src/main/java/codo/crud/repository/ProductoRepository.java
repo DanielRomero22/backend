5e6ece4445e39230d66d3fd02f878c028423cbf5
Dnaiel Romero <romerodaniel@outlook.com>
2022-07-15 20:48:29 -0300

    first commit

---
+++ src/main/java/codo/crud/repository/ProductoRepository.java
@@ -0,0 +1,13 @@
+package codo.crud.repository;
+
+import codo.crud.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+    Optional<Producto> findByNombre(String nombre);
+    boolean existsByNombre(String nombre);
+}
---
+++ src/test/java/codo/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package codo.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
